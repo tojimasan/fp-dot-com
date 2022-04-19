8da79a4cd79df4f549523d487faec7f4c1c914ee
Ushijima Kazuto <k_ushijima@fancs.com>
2022-04-19 20:12:14 +0900

    #57 FPのアソシエーションのテストケースを追加

--- src/spec/factories/financial_planners.rb
+++ src/spec/factories/financial_planners.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :financial_planner do
     sequence(:name) { |n| "FP#{n}" }
+
+    trait(:with_slots) do
+      after(:create) do |financial_planner, evaluator|
+        create_list(:consultation_appointment_slot, 2, financial_planner: financial_planner)
+      end
+    end
   end
 end
--- src/spec/models/financial_planner_spec.rb
+++ src/spec/models/financial_planner_spec.rb
@@ -33,4 +33,15 @@ RSpec.describe FinancialPlanner, type: :model do
       end
     end
   end
+
+  describe '#consultation_appointment_slots' do
+    context '予約されていない枠が複数ある場合' do
+      let!(:financial_planner) { create(:financial_planner) }
+      let!(:unreserved_slot) { create :consultation_appointment_slot, financial_planner: financial_planner, status: 1 }
+      let!(:reserved_slot) { create :consultation_appointment_slot, financial_planner: financial_planner, status: 2 }
+      it '件数が返ること' do
+        expect(financial_planner.consultation_appointment_slots).to eq [unreserved_slot, reserved_slot]
+      end
+    end
+  end
 end
